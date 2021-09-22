c283e8e29be42183ad42bc8d09560ef9df0b610b
taylorreiter <taylorreiter@gmail.com>
2021-09-22 13:06:45 -0700

    make script to grab accessions from species name

---
+++ scripts/grab_species_accessions.R
@@ -0,0 +1,20 @@
+library(readr)
+library(dplyr)
+
+# metadata <- read_tsv("inputs/metadata_small.tsv")
+metadata <- read_tsv(snakemake@input[['metadata']])
+lineages <- read_csv(snakemake@input[['lineages']])
+
+# grab the species of interest
+species_no_space_w <- unlist(snakemake@wildcards[['species']])
+#species_no_space_w <- "s__Cuniculiplasma-divulgatum"
+metadata_w <- metadata %>%
+  filter(species_no_space == species_no_space_w)
+
+# write a tsv file that contains the lineage information for each set of species
+# specified in the metadata file
+
+lineages_w <- lineages %>% 
+  filter(species %in% metadata_w$species)
+
+write_tsv(snakemake@output[["tsv"]])
