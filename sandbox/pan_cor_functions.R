c98805abcb357ba37e0c06a7e3c444cd3362b8ee
taylorreiter <taylorreiter@gmail.com>
2021-10-19 13:38:11 -0700

    add sbox draft

--- sandbox/pan_cor_functions.R
+++ sandbox/pan_cor_functions.R
@@ -6,6 +6,7 @@ library(arrow)
 library(ggplot2)
 library(vegan)
 library(broom)
+library(ggpubr)
 
 
 # functions ---------------------------------------------------------------
@@ -26,7 +27,7 @@ read_roary_presence_absence <- function(path){
 
 read_mers_presence_absence <- function(path) {
   mers <- arrow::read_feather(path)
-  mers$acc <- gsub("_k10_scaled1", "", mers$acc)
+  #mers$acc <- gsub("_[100]", "", mers$acc)
   mers$acc <- make_clean_names(mers$acc)
   mers <- as.data.frame(mers)
   rownames(mers) <- mers$acc
@@ -100,8 +101,8 @@ mantel_roary_and_mers <- function(roary, mers, species){
 # apply -------------------------------------------------------------------
 
 roary <- read_roary_presence_absence(path = "outputs/roary/s__Cuniculiplasma-divulgatum/gene_presence_absence.csv")
-mers <- read_mers_presence_absence(path = "outputs/sourmash_sketch_tables/s__Cuniculiplasma-divulgatum_k10_scaled1_wide.feather")
-
+mers <- read_mers_presence_absence(path = "outputs/sourmash_sketch_tables/s__Cuniculiplasma-divulgatum_k10_scaled100_wide.feather")
+head(mers$acc)
 # correlate number of genes with number of kmers --------------------------
 species <- "s__Cuniculiplasma-divulgatum"
 
@@ -114,3 +115,59 @@ mantel_roary_and_mers(roary = roary, mers = mers, species = species)
 # correlate number of unique genes with number of unique kmers ------------
 
 correlate_unique_per_genome(roary = roary, mers = mers, species = species)
+
+
+# try accumulation curves -------------------------------------------------
+
+correlate_specaccum <- function(roary, mers, species){
+  sp_roary <- specaccum(roary, "exact")
+  sp_roary_df <- data.frame(genomes = sp_roary$sites, genes = sp_roary$richness, sd = sp_roary$sd) 
+
+  sp_mers <- specaccum(mers, "exact")
+  sp_mers_df <- data.frame(genomes = sp_mers$sites, mers = sp_mers$richness, sd = sp_mers$sd) 
+  
+  tmp1 <- data.frame(genomes = sp_roary$sites, units = sp_roary$richness, sd = sp_roary$sd, measure = "genes")
+  tmp2 <-  data.frame(genomes = sp_mers$sites, units = sp_mers$richness, sd = sp_mers$sd, measure = "kmers")
+  both_sp_long <- rbind(tmp1, tmp2)
+  
+  specaccum_plt <- ggplot() +
+    geom_ribbon(data = both_sp_long, aes(x = genomes, y = units, ymin = units - sd, ymax = units + sd, fill = measure), alpha = 1/3) +
+    geom_point(data = both_sp_long, aes(x = genomes, y = units, color = measure), size = 1) + 
+    theme_minimal() +
+    theme(plot.title.position = "plot",
+          legend.position = "top",
+          legend.text = element_text(size = 7),
+          legend.title = element_text(size = 8),
+          axis.title = element_text(size = 8),
+          plot.title = element_text(size = 9),
+          plot.subtitle = element_text(size = 8),
+          axis.text = element_text(size = 7)) +
+    labs(title = species)
+  
+  both_sp_wide <- left_join(sp_mers_df, sp_roary_df, by = "genomes")
+  lm_result <- lm(mers ~ genes, both_sp_wide) %>%
+    glance()
+  
+  r_squared <- round(lm_result$r.squared, digits = 2)
+  p_value <- ifelse(lm_result$p.value > 0.001, 
+                    paste0("= ", round(lm_result$p.value, digits = 3)), 
+                    "< 0.001")
+  
+  specaccum_cor_plt <- ggplot(both_sp_wide, aes(x = genes, y = mers)) +
+    geom_point() +
+    theme_minimal() +
+    theme(plot.title.position = "plot", 
+          axis.title = element_text(size = 8),
+          plot.title = element_text(size = 9),
+          plot.subtitle = element_text(size = 8),
+          axis.text = element_text(size = 7)) +
+    labs(x = "genes", y = "k-mers", 
+         title = "Accumulation curve values per genome added",
+         subtitle = paste0("R-squared:  ", r_squared, " (p ", p_value, ")"))
+  
+  ggarrange(specaccum_plt, specaccum_cor_plt)
+  
+}
+
+correlate_specaccum(roary = roary, mers = mers, species = species)
+
